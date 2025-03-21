9f9ee07d10f207084827479a31bb8746077ad430
wahidfoodloop <wahid@myfoodloop.com>
2025-03-21 14:19:05 +0500

    initial setup

---
+++ storage/framework/views/50886331669786721cd02da738763ea4.php
@@ -0,0 +1,2 @@
+<img src="<?php echo e(asset('brand/logo-mydomotics.png')); ?>" alt="MyDomotics" width="200">
+<?php /**PATH C:\xampp\htdocs\domotics\resources\views/components/application-logo.blade.php ENDPATH**/ ?>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd7f56b995c8cd1c56ced4a99d5e3f8585d621cd2',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -440,9 +440,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd7f56b995c8cd1c56ced4a99d5e3f8585d621cd2',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
