6f6ac3cd72ff7a3e7da7a37c53537e8fce41124e
Zafar Ali <zafaraliab@gmail.com>
2025-05-08 14:34:15 +0500

    changes

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,15 +1,17 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 sm:gap-0 mb-4">
+        <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 {{ __('Utenti') }}
             </h2>
-            <a href="{{ route('users.create') }}"
-                class="text-sm sm:text-base px-4 py-2 bg-indigo-600 text-white rounded-md shadow hover:bg-indigo-700 transition">
-                + Aggiungi un utente
-            </a>
-        </div>
+            <div>
+                <a href="{{ route('users.create') }}"
+                    class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    + Aggiungi un utente
+                </a>
 
+            </div>
+        </div>
     </x-slot>
 
     <main class="mt-6">
@@ -52,9 +54,9 @@
                                                         @csrf
                                                         @method('DELETE')
                                                         <button type="submit"
-                                                            class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+                                                            class="inline-flex items-center px-3 py-2 bg-red-600 hover:bg-red-700 text-white border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition"
                                                             onclick="return confirm('Sei sicuro di voler cancellare questo utente?')">
-                                                            <i class="fas fa-trash-alt mr-2"></i> </button>
+                                                            Cancella</button>
                                                     </form>
                                                 @else<p>L'utente ha preventivi</p>
                                                 @endif
